e6a075b129286384c5328091471e96a66412e9ce
Batr Kanzitelli <kanzitdinov@gmail.com>
2020-10-08 01:53:56 +0200

    new help methods

--- src/utils/helpMethods.ts
+++ src/utils/helpMethods.ts
@@ -1,4 +1,10 @@
 
 export const generateRedditUserString = (user: string) => `u/${user}`;
 
+export const generateSubredditString = (sub: string) => `r/${sub}`;
+
 export const generateRedditPostUrl = (permalink: string) => `https://reddit.com${permalink}`;
+
+export const generateCommentsString = (comments: number) => `${comments} comment${comments !== 1 ? 's' : ''}`;
+
+export const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
