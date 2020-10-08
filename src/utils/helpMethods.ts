57ca8987944a9c5eac3337ad29eced4b42e1fd38
Batr Kanzitelli <kanzitdinov@gmail.com>
2020-10-08 17:55:58 +0200

    sleep & kFormatter added

--- src/utils/helpMethods.ts
+++ src/utils/helpMethods.ts
@@ -8,3 +8,5 @@ export const generateRedditPostUrl = (permalink: string) => `https://reddit.com$
 export const generateCommentsString = (comments: number) => `${comments} comment${comments !== 1 ? 's' : ''}`;
 
 export const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+
+export const kFormatter = (num: number) => Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num);
