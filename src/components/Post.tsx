01ae909f2a42622f38c614a8c26bb148146a2572
Batr Kanzitelli <kanzitdinov@gmail.com>
2020-10-08 17:55:16 +0200

    post item appearance changed a bit

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -6,7 +6,7 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 
 import useStyles from '../utils/useStyles';
-import { generateCommentsString, generateRedditUserString, generateSubredditString } from '../utils/helpMethods';
+import { generateCommentsString, generateRedditUserString, generateSubredditString, kFormatter } from '../utils/helpMethods';
 
 type PostProps = {
   item: RedditPost;
@@ -26,11 +26,13 @@ const Post: React.FC<PostProps> = ({
       <TouchableOpacity onPress={onPress}>
         <View style={styles.buttonContainer}>
           <View style={styles.upsTextContainer}>
-            <Text style={styles.upsText}>{ item.ups }</Text>
+            <Text style={styles.upsText}>{ kFormatter(item.ups) }</Text>
           </View>
           <View style={styles.titlesContainer}>
-            <Text style={styles.subtitle}>Posted by { generateRedditUserString(item.author) } { dayjs(item.created_utc*1000).fromNow() }</Text>
-            { withSub && <Text style={styles.subtitle}>in { generateSubredditString(item.subreddit) }</Text> }
+            <View style={{ flexDirection: 'row' }}>
+              { withSub && <Text style={styles.subreddit}>{ generateSubredditString(item.subreddit) } </Text> }
+              <Text numberOfLines={1} style={styles.subtitle}>Posted by { generateRedditUserString(item.author) } { dayjs(item.created_utc*1000).fromNow() }</Text>
+            </View>
             <Text style={styles.title}>{ item.title }</Text>
             <Text style={styles.subtitle}>{ generateCommentsString(item.num_comments) }</Text>
           </View>
@@ -59,9 +61,16 @@ const _styles = (theme: ThemeType) => StyleSheet.create({
     fontSize: 14,
     color: theme.colors.grey
   },
+  subreddit: {
+    marginVertical: 2,
+    fontSize: 14,
+    color: theme.colors.grey,
+    fontWeight: 'bold',
+  },
   upsTextContainer: {
     minWidth: 30,
-    margin: 2,
+    marginHorizontal: 4,
+    marginVertical: 2,
   },
   upsText: {
     fontSize: 14,
