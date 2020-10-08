569feef7e1cfe550739061ca4d0ecac3e2fed6ab
Batr Kanzitelli <kanzitdinov@gmail.com>
2020-10-08 17:55:26 +0200

    settings

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -3,14 +3,19 @@ import {
     SafeAreaView,
     Text,
     View,
-    StyleSheet,
+    StyleSheet, Share, Linking
 } from 'react-native';
 import { observer } from 'mobx-react';
 import { NavigationFunctionComponent } from 'react-native-navigation';
+import { ScrollView } from 'react-native-gesture-handler';
+import Rate from 'react-native-rate';
+import * as MailComposer from 'expo-mail-composer';
+import { getReadableVersion } from 'react-native-device-info';
 
 import { useStores } from '../stores';
 import Constants from '../utils/constants';
 import useStyles from '../utils/useStyles';
+import { ButtonAction, ButtonTitle } from '../components/Button';
 
 const SettingsScreen: NavigationFunctionComponent = observer(({
   componentId,
@@ -18,13 +23,85 @@ const SettingsScreen: NavigationFunctionComponent = observer(({
   const { } = useStores();
   const { styles } = useStyles(_styles);
 
+  const shareApp = () =>
+    Share.share({ url: Constants.AppUrl });
+
+  const rateApp = () =>
+    Rate.rate(Constants.RateAppConfig, s => {});
+
+  const openSupport = async () =>
+    (await MailComposer.isAvailableAsync())
+      ? MailComposer.composeAsync(Constants.MailComposerOptions)
+      : console.log('MailComposer is not available');
+
+  const openGithub = () =>
+    Linking.openURL('https://github.com/kanzitelli');
+
+  const openArticle = () =>
+    Linking.openURL('https://medium.com/kanzitelli');
+
+  const openWebsite = () =>
+    Linking.openURL(Constants.BaseUrl);
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.counterContainer}>
-        <Text style={styles.text}>
-          { 'About' }
-        </Text>
-      </View>
+      <ScrollView>
+        <View style={styles.section}>
+          <Text style={styles.header}>
+            { 'General' }
+          </Text>
+
+          <ButtonAction
+            title={'Share'}
+            icon={'ios-share-outline'}
+            onPress={shareApp}
+          />
+          <ButtonAction
+            title={'Rate'}
+            icon={'star-outline'}
+            onPress={rateApp}
+          />
+          <ButtonAction
+            title={'Support'}
+            icon={'mail-unread-outline'}
+            onPress={openSupport}
+          />
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.header}>
+            { 'Links' }
+          </Text>
+
+          <ButtonAction
+            title={'Repository'}
+            icon={'logo-github'}
+            onPress={openGithub}
+          />
+          <ButtonAction
+            title={'Article'}
+            icon={'newspaper-outline'}
+            onPress={openArticle}
+          />
+          <ButtonAction
+            title={'Website'}
+            icon={'ios-earth-outline'}
+            onPress={openWebsite}
+          />
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.header}>
+            { 'About' }
+          </Text>
+
+          <ButtonAction
+            disabled
+            title={`Version: ${getReadableVersion()}`}
+            icon={'information-circle-outline'}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 });
@@ -32,21 +109,19 @@ const SettingsScreen: NavigationFunctionComponent = observer(({
 const _styles = (theme: ThemeType) => StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: theme.colors.bg,
   },
-  counterContainer: {
-    padding: theme.sizes.s,
-    flexDirection: 'row',
-    width: '80%',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  section: {
+    padding: theme.sizes.m,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: theme.colors.main,
   },
   text: {
-    fontSize: 64,
+    fontSize: 20,
     margin: theme.sizes.s,
-    textAlign: 'center',
     color: theme.colors.text,
   },
 });
